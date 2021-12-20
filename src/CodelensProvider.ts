ca32c3c3c7fe0f43e55bf79a76d3a68fe2fc5986
Asaf Chen <asafc64@gmail.com>
2021-12-20 11:01:14 +0200

    Using pyright instead of pylance

--- src/CodelensProvider.ts
+++ src/CodelensProvider.ts
@@ -1,12 +1,9 @@
 import * as vscode from 'vscode';
 import {
-    CancellationToken,
     DocumentSymbol,
     DocumentSymbolParams,
     DocumentSymbolRequest,
     SymbolInformation,
-    ReferenceParams,
-    Command,
     SymbolKind
 } from "vscode-languageclient";
 import { LanguageClient } from 'vscode-languageclient/node';
@@ -30,16 +27,17 @@ export class CodelensProvider implements vscode.CodeLensProvider {
     buildLens(parentPath: string, symbols: DocumentSymbol[]) : vscode.CodeLens[]
     {
         let codeLenses : vscode.CodeLens[] = [];
-        symbols.forEach(docSym => {
+   
+        for (let sym of symbols) {
 
-            let path = (parentPath ? parentPath+'.' : '')+docSym.name
+            let path = (parentPath ? parentPath+'.' : '')+sym.name
 
-            if (docSym.kind == SymbolKind.Function ||
-                docSym.kind == SymbolKind.Method)
+            if (sym.kind == SymbolKind.Function ||
+                sym.kind == SymbolKind.Method)
             {
                 let r = new vscode.Range(
-                    new vscode.Position(docSym.range.start.line, docSym.range.start.character),
-                    new vscode.Position(docSym.range.end.line, docSym.range.end.character));
+                    new vscode.Position(sym.range.start.line, sym.range.start.character),
+                    new vscode.Position(sym.range.end.line, sym.range.end.character));
 
                 let c: vscode.Command ={
                     title: path,
@@ -50,42 +48,42 @@ export class CodelensProvider implements vscode.CodeLensProvider {
                 codeLenses.push(new vscode.CodeLens(r, c));
             }
 
-            if(docSym.children){
-                codeLenses = codeLenses.concat(this.buildLens(path, docSym.children));
+            if(sym.children){
+                codeLenses = codeLenses.concat(this.buildLens(path, sym.children));
             }
-        }); 
+        }
+
         return codeLenses;
     }
 
     public async provideCodeLenses(document: vscode.TextDocument, token: vscode.CancellationToken): Promise<vscode.CodeLens[]> {
 
-        if (vscode.workspace.getConfiguration("digma").get("enableCodeLens", true)) {
+        if (!vscode.workspace.getConfiguration("digma").get("enableCodeLens", true)) 
+            return [];
         
-            let codeLenses : vscode.CodeLens[] = [];
+        let codeLenses : vscode.CodeLens[] = [];
 
-            const args: DocumentSymbolParams = {
-                textDocument: this._languageClient.code2ProtocolConverter.asTextDocumentIdentifier(document),
-            };
-            const result = await this._languageClient.sendRequest(
-                DocumentSymbolRequest.type,
-                args,
-                token);
+        const args: DocumentSymbolParams = {
+            textDocument: this._languageClient.code2ProtocolConverter.asTextDocumentIdentifier(document),
+        };
+        const result = await this._languageClient.sendRequest(
+            DocumentSymbolRequest.type,
+            args,
+            token);
 
-            if (result && result.length) {
-                if ((result[0] as any).range) {
-                    // Document symbols
-                    const docSymbols = result as DocumentSymbol[];
-                    codeLenses = this.buildLens('', docSymbols);
-                } else {
-                    // Document symbols
-                    const symbols = result as SymbolInformation[];
-                    // TODO: ?
-                }
+        if (result && result.length) {
+            if ((result[0] as any).range) {
+                // Document symbols
+                const docSymbols = result as DocumentSymbol[];
+                codeLenses = this.buildLens('', docSymbols);
+            } else {
+                // Document symbols
+                const symbols = result as SymbolInformation[];
+                // TODO: ?
             }
-
-            return codeLenses;
         }
-        return [];
+
+        return codeLenses;
     }
 
     public resolveCodeLens(codeLens: vscode.CodeLens, token: vscode.CancellationToken) {
