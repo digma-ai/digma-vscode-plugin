4c847d5872d64d19e0b22c6ece9d39b057587e65
asafc <asafc64@gmail.com>
2022-03-09 14:46:15 +0200

    cleanup

--- assets/common.js
+++
@@ -1,4 +0,0 @@
-$(document).on('click', '.has-items', function(){
-    $(this).toggleClass('active');
-    $(this).find('.codicon:first-of-type').toggleClass('codicon-chevron-right codicon-chevron-down');
-});
