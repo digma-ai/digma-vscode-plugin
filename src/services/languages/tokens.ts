0a9492691743920062980ec17810b688436a1781
Noam Kfir <noam@digma.ai>
2022-04-25 02:50:33 +0300

    Introduce token matching functions

--- src/services/languages/tokens.ts
+++ src/services/languages/tokens.ts
@@ -7,6 +7,11 @@ export interface Token {
     modifiers: string[];
 }
 
+export interface TokenMatcher {
+    type?: TokenType;
+    text?: string;
+}
+
 export enum TokenType {
     class = 'class',
     interface = 'interface',
@@ -31,4 +36,55 @@ export enum TokenType {
     string = 'string',
     plainKeyword = 'plainKeyword',
     namespace = 'namespace'
+}
+
+export function matchTokenSequence(tokens: Token[], matchers: TokenMatcher[]): boolean {
+    if(!tokens || !matchers) {
+        return false;
+    }
+
+    if(tokens.length === 0 && matchers.length === 0) {
+        return true;
+    }
+
+    if(tokens.length === 0 || matchers.length === 0) {
+        return false;
+    }
+
+    for(let tokenIndex = 0; tokenIndex < tokens.length - matchers.length; tokenIndex++) {
+        let isPartialMatch = true;
+        for(let matcherIndex = 0; matcherIndex < matchers.length; matcherIndex++) {
+            const token = tokens[tokenIndex + matcherIndex];
+            const matcher = matchers[matcherIndex];
+            if(!matchToken(token, matcher)) {
+                isPartialMatch = false;
+                break;
+            }
+        }
+        if(isPartialMatch) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+export function matchToken(token: Token, match: TokenMatcher) {
+    if(!token || !match) {
+        return false;
+    }
+
+    if(token === match) {
+        return true;
+    }
+
+    if(match.text && match.text !== token.text) {
+        return false;
+    }
+
+    if(match.type && match.type !== token.type) {
+        return false;
+    }
+
+    return true;
 }
