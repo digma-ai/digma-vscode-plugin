5b4bc7c4d50a24efb23211293b5cc088d37e7d5b
Noam Kfir <noam@digma.ai>
2022-04-30 01:07:16 +0300

    Recursively search for base class in mvc endpoint extractor

--- src/services/languages/csharp/AspNetCoreMvcEndpointExtractor.ts
+++ src/services/languages/csharp/AspNetCoreMvcEndpointExtractor.ts
@@ -1,7 +1,7 @@
 import * as vscode from 'vscode';
 import { DocumentInfoProvider } from './../../documentInfoProvider';
 import { CodeInspector } from '../../codeInspector';
-import { SymbolTree } from './../symbolProvider';
+import { SymbolTree, SymbolProvider } from './../symbolProvider';
 import { Token, TokenType, matchTokenSequence } from '../tokens';
 import { EndpointInfo, IEndpointExtractor, SymbolInfo } from '../extractors';
 
@@ -22,28 +22,12 @@ export class AspNetCoreMvcEndpointExtractor implements IEndpointExtractor {
         const classes = Array.from(this._codeInspector.getAllSymbolsOfKind(symbolTrees, vscode.SymbolKind.Class));
 
         for (const currentClass of classes) {
+
             const { line, character } = currentClass.selectionRange.start;
-            const position = new vscode.Position(line, character);
-
-            // const definition = await this._codeInspector.getExecuteDefinitionMethodInfo(document, position, documentInfoProvider);
-            // const definition = await this._codeInspector.getDefinition(document, position);
-
-            const openBraceToken = tokens.find(t => t.range.start.isAfter(position) && t.type === TokenType.punctuation && t.text === '{');
-            if(!openBraceToken) {
-                continue;
-            }
-
-            const baseClassToken = tokens.find(
-                t =>
-                    t.range.start.isAfter(position)
-                    && t.range.end.isBefore(openBraceToken.range.start)
-                    && t.type === TokenType.class
-            );
-            if(!baseClassToken) {
-                continue;
-            }
-
-            if(baseClassToken.text === 'ControllerBase') {
+            const classNamePosition = new vscode.Position(line, character);
+    
+            const derivesFromControllerBase = await this.derivesFrom(classNamePosition, 'ControllerBase', document, tokens, documentInfoProvider.symbolProvider);
+            if(derivesFromControllerBase) {
                 const children = currentClass.children;
                 if(!children) {
                     continue;
@@ -76,5 +60,51 @@ export class AspNetCoreMvcEndpointExtractor implements IEndpointExtractor {
         });
 
         return results;
+    }
+
+    private async derivesFrom(
+        classNamePosition: vscode.Position,
+        ancestorName: string,
+        document: vscode.TextDocument,
+        tokens: Token[],
+        symbolProvider: SymbolProvider,
+    ): Promise<boolean> {
+        const openBraceToken = tokens.find(
+            token =>
+                token.range.start.isAfter(classNamePosition)
+                && token.type === TokenType.punctuation
+                && token.text === '{'
+        );
+        if(!openBraceToken) {
+            return false;
+        }
+
+        const parentToken = tokens.find(
+            token =>
+                token.range.start.isAfter(classNamePosition)
+                && token.range.end.isBefore(openBraceToken.range.start)
+                && token.type === TokenType.class
+        );
+        if(!parentToken) {
+            return false;
+        }
+
+        if(parentToken.text === ancestorName) {
+            return true;
+        }
+
+        const parentInfo = await this._codeInspector.getTokensFromSymbolProvider(document, parentToken.range.start, symbolProvider);
+        if(!parentInfo) {
+            return false;
+        }
+
+        const parentDocument = parentInfo.document;
+        const parentSymbolTree = await symbolProvider.getSymbolTree(parentDocument);
+        if(!parentSymbolTree) {
+            return false;
+        }
+
+        const parentDerivesFromAncestor = this.derivesFrom(parentInfo.location.range.start, ancestorName, parentDocument, parentInfo.tokens, symbolProvider);
+        return parentDerivesFromAncestor;
     }
 }
