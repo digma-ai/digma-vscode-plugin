8b03124ced32a4a52a40f16174b2361a1d8a51ec
Shay Keren <shay.keren@gmail.com>
2022-06-07 17:14:34 +0300

    cleanup

--- src/services/languages/symbolProvider.ts
+++ src/services/languages/symbolProvider.ts
@@ -7,33 +7,6 @@ import { CodeInspector } from '../codeInspector';
 import { EndpointInfo, ILanguageExtractor, SpanInfo, SymbolInfo } from './extractors';
 import { Token, TokenType } from './tokens';
 
-interface IParsedToken {
-	line: number;
-	startCharacter: number;
-	length: number;
-	tokenType: string;
-	tokenModifiers: string[];
-}
-
-const tokenTypes = new Map<string, number>();
-const tokenModifiers = new Map<string, number>();
-const legend = (function () {
-	const tokenTypesLegend = [
-		'comment', 'string', 'keyword', 'number', 'regexp', 'operator', 'namespace',
-		'type', 'struct', 'class', 'interface', 'enum', 'typeParameter', 'function',
-		'method', 'decorator', 'macro', 'variable', 'parameter', 'property', 'label'
-	];
-	tokenTypesLegend.forEach((tokenType, index) => tokenTypes.set(tokenType, index));
-
-	const tokenModifiersLegend = [
-		'declaration', 'documentation', 'readonly', 'static', 'abstract', 'deprecated',
-		'modification', 'async'
-	];
-	tokenModifiersLegend.forEach((tokenModifier, index) => tokenModifiers.set(tokenModifier, index));
-
-	return new vscode.SemanticTokensLegend(tokenTypesLegend, tokenModifiersLegend);
-})();
-
 export function trendToCodIcon(trend: number): string 
 {
     if(trend < 0)
@@ -178,63 +151,6 @@ export class SymbolProvider
         
         return [];
     }
-
-    private _parseTextToken(text: string): { tokenType: string; tokenModifiers: string[]; } {
-		const parts = text.split('.');
-		return {
-			tokenType: parts[0],
-			tokenModifiers: parts.slice(1)
-		};
-	}
-    private _encodeTokenModifiers(strTokenModifiers: string[]): number {
-		let result = 0;
-		for (let i = 0; i < strTokenModifiers.length; i++) {
-			const tokenModifier = strTokenModifiers[i];
-			if (tokenModifiers.has(tokenModifier)) {
-				result = result | (1 << tokenModifiers.get(tokenModifier)!);
-			} else if (tokenModifier === 'notInLegend') {
-				result = result | (1 << tokenModifiers.size + 2);
-			}
-		}
-		return result;
-	}
-    private _parseText(text: string): IParsedToken[] {
-		const r: IParsedToken[] = [];
-		const lines = text.split(/\r\n|\r|\n/);
-		for (let i = 0; i < lines.length; i++) {
-			const line = lines[i];
-			let currentOffset = 0;
-			do {
-				const openOffset = line.indexOf('[', currentOffset);
-				if (openOffset === -1) {
-					break;
-				}
-				const closeOffset = line.indexOf(']', openOffset);
-				if (closeOffset === -1) {
-					break;
-				}
-				const tokenData = this._parseTextToken(line.substring(openOffset + 1, closeOffset));
-				r.push({
-					line: i,
-					startCharacter: openOffset + 1,
-					length: closeOffset - openOffset - 1,
-					tokenType: tokenData.tokenType,
-					tokenModifiers: tokenData.tokenModifiers
-				});
-				currentOffset = closeOffset;
-			} while (true);
-		}
-		return r;
-	}
-    private _encodeTokenType(tokenType: string): number {
-		if (tokenTypes.has(tokenType)) {
-			return tokenTypes.get(tokenType)!;
-		} else if (tokenType === 'notInLegend') {
-			return tokenTypes.size + 2;
-		}
-		return 0;
-	}
-
     public async getTokens(document: vscode.TextDocument, range?: vscode.Range): Promise<Token[]> {
         let tokes: Token[] = [];
         try {
@@ -271,14 +187,7 @@ export class SymbolProvider
             if(!semanticTokens) {
                 return tokes;
             }
-            let config = this.getConfig('gopls', document.uri)['ui.semanticTokens'];
-
-            const extension = vscode.extensions.getExtension("golang.go");
-            if(extension)
-            {
-                let c = extension.exports.settings.getExecutionCommand("go",document.uri);
-            }
-
+          
             let line = 0;
             let char = 0;
             for(let i = 0; i < semanticTokens.data.length; i += 5) {
@@ -324,17 +233,6 @@ export class SymbolProvider
             return;
         }
         return supportedLanguage;
-    }
-
-    private getConfig(section: string, uri?: vscode.Uri | null) {
-        if (!uri) {
-            if (vscode.window.activeTextEditor) {
-                uri = vscode.window.activeTextEditor.document.uri;
-            } else {
-                uri = null;
-            }
-        }
-        return vscode.workspace.getConfiguration(section, uri);
     }
 
     private async loadRequiredExtension(language: ILanguageExtractor) : Promise<boolean>
