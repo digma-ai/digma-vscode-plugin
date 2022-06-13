3e9e86458500956004839b61142b2e4ca7baed51
arik-dig <asher@digma.ai>
2022-06-13 16:58:43 +0300

    overcome the problem with URI

--- src/services/languages/go/methodExtractor.ts
+++ src/services/languages/go/methodExtractor.ts
@@ -7,18 +7,20 @@ import { Logger } from '../../logger';
 export class GoMethodExtractor implements IMethodExtractor{
     public async extractMethods(document: vscode.TextDocument, docSymbols: DocumentSymbol[]): Promise<SymbolInfo[]> {
         const methodSymbols = docSymbols.filter(s => s.kind+1 === SymbolKind.Method || s.kind+1 === SymbolKind.Function); 
-        if(!methodSymbols.length)
+        if(!methodSymbols.length) {
             return [];
+        }
 
-        const modFiles = await vscode.workspace.findFiles('**/go.mod');
-        const modFile = modFiles.find(f => document.uri.path.startsWith(path.dirname(f.path)))
+        const modFiles = await (await vscode.workspace.findFiles('**/go.mod')).map( x => x.fsPath);
+        const normDocPath = document.uri.fsPath;
+        const modFile = modFiles.find(f => normDocPath.startsWith(path.dirname(f)));
         if(!modFile){
             Logger.warn(`Could not resolve mod file for '${document.uri.path}'`)
             return [];
         }
 
-        const modFolder = path.dirname(modFile.path);
-        const docFolder = path.dirname(document.uri.path);
+        const modFolder = path.dirname(modFile);
+        const docFolder = path.dirname(normDocPath);
         let packageName = '';
         if(modFolder === docFolder){
             const match = document.getText().match(/^package (.+)$/m);
@@ -32,13 +34,16 @@ export class GoMethodExtractor implements IMethodExtractor{
             const modDocument = await vscode.workspace.openTextDocument(modFile);
             const match = modDocument.getText().match(/^module (.+)$/m);
             if(!match){
-                Logger.warn(`Could not found module name in '${modFile.path}'`)
+                Logger.warn(`Could not found module name in '${modFile}'`)
                 return [];
             }
             packageName = match[1] 
             
-            if(modFolder !== docFolder)
-                packageName += '/' + path.relative(modFolder, docFolder);
+            if (modFolder !== docFolder) {
+                const relative = path.relative(modFolder, docFolder)
+                    .replace('\\', '/'); // get rid of windows backslashes
+                packageName += '/' + relative;
+            }
         }
 
         const methods: SymbolInfo[] = methodSymbols.map(s => {
