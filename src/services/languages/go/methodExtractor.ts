95aefd0f7812852f3692853f390c1bed623aaf73
Shay Keren <shay.keren@gmail.com>
2022-06-14 15:56:54 +0300

    golang: codeobject for functions under main package (#120)

--- src/services/languages/go/methodExtractor.ts
+++ src/services/languages/go/methodExtractor.ts
@@ -4,55 +4,61 @@ import { DocumentSymbol, SymbolKind } from "vscode-languageclient";
 import { IMethodExtractor, SymbolInfo } from "../extractors";
 import { Logger } from '../../logger';
 
-export class GoMethodExtractor implements IMethodExtractor {
+export class GoMethodExtractor implements IMethodExtractor{
+    private async getModuleName(modFile:vscode.Uri): Promise<string| undefined>{
+        const modDocument = await vscode.workspace.openTextDocument(modFile);
+        const match = modDocument.getText().match(/^module (.+)$/m);
+        if(!match){
+            Logger.warn(`Could not found module name in '${modFile.path}'`);
+            return undefined;
+        }
+        return match[1];
+    }
+
+    private getPackageDefinationName(document: vscode.TextDocument): string| undefined{
+        const match = document.getText().match(/^package (.+)$/m);
+        if(!match){
+            Logger.warn(`Could not found packakge name in '${document.uri.path}'`);
+            return undefined;
+        }
+        return match[1]; 
+    }
+
     public async extractMethods(document: vscode.TextDocument, docSymbols: DocumentSymbol[]): Promise<SymbolInfo[]> {
-        const methodSymbols = docSymbols.filter(s => s.kind + 1 === SymbolKind.Method || s.kind + 1 === SymbolKind.Function);
-        if (!methodSymbols.length) {
+        const methodSymbols = docSymbols.filter(s => s.kind+1 === SymbolKind.Method || s.kind+1 === SymbolKind.Function); 
+        if(!methodSymbols.length){
             return [];
         }
-
-        const modFiles = await (await vscode.workspace.findFiles('**/go.mod')).map(x => x.fsPath);
-        const normDocPath = document.uri.fsPath;
-        const modFile = modFiles.find(f => normDocPath.startsWith(path.dirname(f)));
-        if (!modFile) {
+            
+        const modFiles = await vscode.workspace.findFiles('**/go.mod');
+        const modFile = modFiles.find(f => document.uri.path.startsWith(path.dirname(f.path)));
+        if(!modFile){
             Logger.warn(`Could not resolve mod file for '${document.uri.path}'`);
             return [];
         }
 
-        const modFolder = path.dirname(modFile);
-        const docFolder = path.dirname(normDocPath);
-        let packageName = '';
-        if (modFolder === docFolder) {
-            const match = document.getText().match(/^package (.+)$/m);
-            if (!match) {
-                Logger.warn(`Could not found packakge name in '${document.uri.path}'`);
-                return [];
-            }
-            packageName = match[1];
+        const moduleName = await this.getModuleName(modFile);
+        if(!moduleName){
+            return [];
         }
-        if (!packageName || packageName !== 'main') {
-            const modDocument = await vscode.workspace.openTextDocument(modFile);
-            const match = modDocument.getText().match(/^module (.+)$/m);
-            if (!match) {
-                Logger.warn(`Could not found module name in '${modFile}'`);
-                return [];
-            }
-            packageName = match[1];
-
-            if (modFolder !== docFolder) {
-                const relative = path.relative(modFolder, docFolder)
-                    .replaceAll('\\', '/'); // get rid of windows backslashes
-                packageName += '/' + relative;
-            }
+        const packageDefinitionName = this.getPackageDefinationName(document);
+        if(!packageDefinitionName){
+            return [];
         }
+        const modFolder = path.dirname(modFile.path);
+        const docFolder = path.dirname(document.uri.path);
 
+        let packagePath = `${moduleName}/${path.relative(modFolder, docFolder.replaceAll('\\', '/'))}`; // get rid of windows backslashes
+        if(packageDefinitionName === "main"){
+            packagePath = moduleName;
+        }
         const methods: SymbolInfo[] = methodSymbols.map(s => {
             return {
-                id: packageName + '$_$' + s.name,
+                id: packageDefinitionName === "main" ? packagePath + '$_$' + `main.${s.name}` : packagePath + '$_$' + s.name,
                 name: s.name,
-                displayName: packageName.split('/').lastOrDefault() + '.' + s.name,
+                displayName: packagePath.split('/').lastOrDefault() + '.' + s.name,
                 documentUri: document.uri,
-                codeLocation: packageName,
+                codeLocation: packagePath,
                 range: new vscode.Range(
                     new vscode.Position(s.range.start.line, s.range.start.character),
                     new vscode.Position(s.range.end.line, s.range.end.character)
