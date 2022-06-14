98078e5752c055b873ca1fdcae454f6e5b2e6577
Shay Keren <shay.keren@gmail.com>
2022-06-14 15:45:52 +0300

    golang: codeobject for functions under main package

--- src/services/languages/go/methodExtractor.ts
+++ src/services/languages/go/methodExtractor.ts
@@ -4,12 +4,33 @@ import { DocumentSymbol, SymbolKind } from "vscode-languageclient";
 import { IMethodExtractor, SymbolInfo } from "../extractors";
 import { Logger } from '../../logger';
 
+
 export class GoMethodExtractor implements IMethodExtractor{
+    private async getModuleName(modFile:vscode.Uri): Promise<string| undefined>{
+        const modDocument = await vscode.workspace.openTextDocument(modFile);
+        const match = modDocument.getText().match(/^module (.+)$/m);
+        if(!match){
+            Logger.warn(`Could not found module name in '${modFile.path}'`);
+            return undefined;
+        }
+        return match[1];
+    }
+
+    private getPackageDefinationName(document: vscode.TextDocument): string| undefined{
+        const match = document.getText().match(/^package (.+)$/m);
+        if(!match){
+            Logger.warn(`Could not found packakge name in '${document.uri.path}'`);
+            return undefined;
+        }
+        return match[1]; 
+    }
+
     public async extractMethods(document: vscode.TextDocument, docSymbols: DocumentSymbol[]): Promise<SymbolInfo[]> {
         const methodSymbols = docSymbols.filter(s => s.kind+1 === SymbolKind.Method || s.kind+1 === SymbolKind.Function); 
-        if(!methodSymbols.length)
+        if(!methodSymbols.length){
             return [];
-
+        }
+           
         const modFiles = await vscode.workspace.findFiles('**/go.mod');
         const modFile = modFiles.find(f => document.uri.path.startsWith(path.dirname(f.path)))
         if(!modFile){
@@ -17,37 +38,32 @@ export class GoMethodExtractor implements IMethodExtractor{
             return [];
         }
 
+        const moduleName = await this.getModuleName(modFile);
+        if(!moduleName){
+            return [];
+        }
+
+        const packageDefinitionName = this.getPackageDefinationName(document);
+        if(!packageDefinitionName){
+            return [];
+        }
+
+
         const modFolder = path.dirname(modFile.path);
         const docFolder = path.dirname(document.uri.path);
-        let packageName = '';
-        if(modFolder === docFolder){
-            const match = document.getText().match(/^package (.+)$/m);
-            if(!match){
-                Logger.warn(`Could not found packakge name in '${document.uri.path}'`)
-                return [];
-            }
-            packageName = match[1]; 
-        }
-        if(!packageName || packageName !== 'main'){
-            const modDocument = await vscode.workspace.openTextDocument(modFile);
-            const match = modDocument.getText().match(/^module (.+)$/m);
-            if(!match){
-                Logger.warn(`Could not found module name in '${modFile.path}'`)
-                return [];
-            }
-            packageName = match[1] 
-            
-            if(modFolder !== docFolder)
-                packageName += '/' + path.relative(modFolder, docFolder);
+
+        let packagePath = `${moduleName}/${path.relative(modFolder, docFolder)}`;
+        if(packageDefinitionName === "main"){
+            packagePath = `${moduleName}`;
         }
 
         const methods: SymbolInfo[] = methodSymbols.map(s => {
             return {
-                id: packageName + '$_$' + s.name,
+                id: packageDefinitionName === "main" ? packagePath + '$_$' + `main.${s.name}` : packagePath + '$_$' + s.name,
                 name: s.name,
-                displayName: packageName.split('/').lastOrDefault() + '.' + s.name,
+                displayName: packagePath.split('/').lastOrDefault() + '.' + s.name,
                 documentUri: document.uri,
-                codeLocation: packageName,
+                codeLocation: packagePath,
                 range: new vscode.Range(
                     new vscode.Position(s.range.start.line, s.range.start.character),
                     new vscode.Position(s.range.end.line, s.range.end.character)
