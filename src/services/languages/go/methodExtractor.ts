615efee0f565886fb746b5a5cfdb123822e5367d
arik-dig <asher@digma.ai>
2022-06-15 15:50:50 +0300

    use fsPath as been in PR #118

--- src/services/languages/go/methodExtractor.ts
+++ src/services/languages/go/methodExtractor.ts
@@ -31,7 +31,7 @@ export class GoMethodExtractor implements IMethodExtractor{
         }
             
         const modFiles = await vscode.workspace.findFiles('**/go.mod');
-        const modFile = modFiles.find(f => document.uri.path.startsWith(path.dirname(f.path)));
+        const modFile = modFiles.find(f => document.uri.fsPath.startsWith(path.dirname(f.fsPath)));
         if(!modFile){
             Logger.warn(`Could not resolve mod file for '${document.uri.path}'`);
             return [];
@@ -45,8 +45,8 @@ export class GoMethodExtractor implements IMethodExtractor{
         if(!packageDefinitionName){
             return [];
         }
-        const modFolder = path.dirname(modFile.path);
-        const docFolder = path.dirname(document.uri.path);
+        const modFolder = path.dirname(modFile.fsPath);
+        const docFolder = path.dirname(document.uri.fsPath);
 
         const relative = path.relative(modFolder, docFolder)
             .replaceAll('\\', '/'); // get rid of windows backslashes
