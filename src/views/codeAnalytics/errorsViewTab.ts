0df0d047e5a0dad18c2ce88d71db4601e2648873
Roni Dover <roni.dover@gmail.com>
2022-06-20 15:17:20 -0700

    small change to message

--- src/views/codeAnalytics/errorsViewTab.ts
+++ src/views/codeAnalytics/errorsViewTab.ts
@@ -356,7 +356,7 @@ class HtmlBuilder
 {
     public static buildErrorItems(codeObject: CodeObjectInfo, errors: CodeObjectError[]): string{
         if(!errors.length){
-            return HtmlHelper.getInfoMessage("No errors flow through this code object.");
+            return HtmlHelper.getInfoMessage("No errors recorded here yet.");
         }
         
         let html = '';
