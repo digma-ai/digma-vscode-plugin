00b49e1f85514536a5db7221d7b85ce08969c081
arik-dig <asher@digma.ai>
2022-07-17 18:50:30 +0300

    CSharpParametersExtractor almost ready - need to add the array part

--- src/services/languages/csharp/parametersExtractor.ts
+++ src/services/languages/csharp/parametersExtractor.ts
@@ -38,8 +38,8 @@ export class CSharpParametersExtractor implements IParametersExtractor {
                 parameterStartIx = ix + 2;
             }
         }
-        //TODO: impl
-        return new Array();
+
+        return params;
     }
 
 
@@ -52,8 +52,15 @@ export class CSharpParametersExtractor implements IParametersExtractor {
                 return this.buildParamInfo(state, token);
                 //break;
             }
-            if (token.type === TokenType.plainKeyword) {
-                state.addPlainKeyword(txt);
+            if (token.type === TokenType.plainKeyword
+                || token.type === TokenType.interface
+                || token.type === TokenType.class
+            ) {
+                if (state.withinArray || state.genericsCount > 0) {
+                    // skip
+                } else {
+                    state.addPlainKeyword(txt);
+                }
             }
             if (token.type === TokenType.punctuation) {
                 if (txt === "(") {
@@ -131,7 +138,7 @@ export class TypeParsingState {
     }
 
     public addPlainKeyword(value: string) {
-        if (this.typeNameStr === "") {
+        if (this.typeNameStr !== "") {
             this.kindStr = this.typeNameStr;
         }
         this.typeNameStr = value;
@@ -145,8 +152,83 @@ export class TypeParsingState {
         this.typeNameStr = "";
     }
 
-    public evalType(): string {
-        //TODO: real impl
-        return this.typeNameStr;
+    public typeNameShortOnly(): string {
+        var typeFqn = BuiltIn.resolveIfPossible(this.typeNameStr);
+        // take only the last part
+        var shortName: string;
+        const lastIndexOfDot = typeFqn.lastIndexOf('.');
+        if (lastIndexOfDot >= 0) {
+            shortName = typeFqn.substring(lastIndexOfDot + 1);
+        } else {
+            shortName = typeFqn;
+        }
+        return shortName;
     }
+
+    public refSignIfNeeded(): string {
+        if (this.kindStr === "ref"
+            || this.kindStr === "out") {
+            return "&";
+        }
+        return "";
+    }
+
+    public genericsSignIfNeeded(): string {
+        if (this.genericsCount > 0) {
+            return "`" + this.genericsCount;
+        }
+        return "";
+    }
+
+    public arraysPartIfNeeded(): string {
+        //TODO impl
+        return "";
+    }
+
+    public evalType(): string {
+        var theVal: string = "";
+        theVal += this.typeNameShortOnly();
+        theVal += this.genericsSignIfNeeded();
+        theVal += this.arraysPartIfNeeded();
+        theVal += this.refSignIfNeeded();
+        return theVal;
+    }
+}
+
+class BuiltIn {
+
+    // see https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/built-in-types
+    static primitiveMap: Map<string, string> = new Map();
+
+    static {
+        //
+        BuiltIn.primitiveMap.set("bool", "System.Boolean");
+        BuiltIn.primitiveMap.set("byte", "System.Byte");
+        BuiltIn.primitiveMap.set("sbyte", "System.SByte");
+        BuiltIn.primitiveMap.set("char", "System.Char");
+        BuiltIn.primitiveMap.set("decimal", "System.Decimal");
+        BuiltIn.primitiveMap.set("double", "System.Double");
+        BuiltIn.primitiveMap.set("float", "System.Single");
+        BuiltIn.primitiveMap.set("int", "System.Int32");
+        BuiltIn.primitiveMap.set("uint", "System.UInt32");
+        BuiltIn.primitiveMap.set("nint", "System.IntPtr");
+        BuiltIn.primitiveMap.set("nuint", "System.UIntPtr");
+        BuiltIn.primitiveMap.set("long", "System.Int64");
+        BuiltIn.primitiveMap.set("ulong", "System.UInt64");
+        BuiltIn.primitiveMap.set("short", "System.Int16");
+        BuiltIn.primitiveMap.set("ushort", "System.UInt16");
+        //
+        BuiltIn.primitiveMap.set("object", "System.Object");
+        BuiltIn.primitiveMap.set("string", "System.String");
+        BuiltIn.primitiveMap.set("dynamic", "System.Object");
+    }
+
+    public static resolveIfPossible(shortName: string): string {
+        var resolved = BuiltIn.primitiveMap.get(shortName);
+        if (resolved) {
+            return resolved;
+        }
+        return shortName;
+    }
+
 }
