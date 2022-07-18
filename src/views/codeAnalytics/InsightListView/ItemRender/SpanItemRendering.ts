4ec9821fb864380b2f5b65d824f985461f68acc3
Roni Dover <roni.dover@gmail.com>
2022-07-17 18:26:23 -0700

    fixed styling

--- src/views/codeAnalytics/InsightListView/ItemRender/SpanItemRendering.ts
+++ src/views/codeAnalytics/InsightListView/ItemRender/SpanItemRendering.ts
@@ -108,7 +108,7 @@ export class SpanItemHtmlRendering{
             const traceIdAtt = `data-trace-id="${traceIds.join(",")}"`;
 
             traceHtml=`
-            <span style="padding-left: 10px;" class="trace-link link" data-jaeger-address="${Settings.jaegerAddress.value}" data-span-name="${insight.span}" 
+            <span  class="insight-main-value trace-link link" data-jaeger-address="${Settings.jaegerAddress.value}" data-span-name="${insight.span}" 
                 ${traceLabelsAtt} ${traceIdAtt} >
             Compare
             </span> 
